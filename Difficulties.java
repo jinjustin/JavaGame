ad30488667b73c9510045d61bb886033486dd14b
JJ <jindefery@gmail.com>
2018-04-19 22:34:31 +0700

    เพิ่มคลาสความยาก
    แต่ยังไม่ได้เอาไปใช้ทำอะไรนะ 555

---
+++ Difficulties.java
@@ -0,0 +1,22 @@
+package astroid;
+
+public class Difficulties {
+    private char difficult;
+    
+    public Difficulties(){
+        
+    }
+    
+    public Difficulties(char difficult){
+        this.difficult = difficult;
+    }
+
+    public int getDifficult() {
+        return difficult;
+    }
+
+    public void setDifficult(char difficult) {
+        this.difficult = difficult;
+    }
+    
+}
